29250d51d937bff0069e5c7e051ba8a12aa05fc5
SiddarthaCMM <c.meza.isw@unipolidgo.edu.mx>
2025-03-04 12:05:22 -0600

    Register y Login Agregado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,34 +8,37 @@
         <div class="login-container">
             <h1>¡Bienvenido!</h1>
             <h2>Iniciar Sesion</h2>
-            <form>
-                <label for="email">Email</label>
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-
-                <label for="password">Contraseña</label>
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-
-                <label class="labelRecuerdame">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>Recuerdame
-                </label>
-
-                <button type="submit">{{ __('Login') }}</button>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                    <label for="email">Email</label>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 
-                @if (Route::has('password.request'))
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        {{ __('Forgot Your Password?') }}
-                    </a>
-                @endif
+                    <label for="password">Contraseña</label>
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+
+                    <div class="form-check">
+                        <label class="labelRecuerdame">
+                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>Recuerdame
+                        </label>
+                    </div>
+
+                    <button id="submitButton" type="submit">{{ __('Login') }}</button>
+                
+                    @if (Route::has('password.request'))
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ __('Forgot Your Password?') }}
+                        </a>
+                    @endif
             </form>
             <p>O continua con</p>
             <div class="social-login">
@@ -43,12 +46,17 @@
                 <button class="facebook">f</button>
                 <button class="apple"></button>
             </div>
-        </div> 
 
+            <div>
+                <div class="user"><span class="newUser">¿Olvidaste tu contraseña?</span></div>
+            </div>
+        </div> 
+        
         <div class ="register-container">
             <h1>¡Bienvenido!</h1>
             <h2>Registrate</h2>
-            <form>
+            <form method="POST" action="{{ route('register') }}">
+            @csrf
                 <label for="email">Email</label>
                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                 @error('email')
@@ -81,6 +89,7 @@
             </form>
             <div class="user">¿Ya tienes cuenta? <span class="existingUser">Inicia Sesion</span></div>
         </div>
+
         <script>
             function togglePassword() {
                 var passwordInput = document.getElementById("contrasena");
